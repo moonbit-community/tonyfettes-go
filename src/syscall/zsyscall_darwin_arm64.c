cd8e044f164ac02a01c8e85c0a5d06b29eff1143
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-09-12 14:37:54 +0800

    feat: File::close

--- src/syscall/zsyscall_darwin_arm64.c
+++ src/syscall/zsyscall_darwin_arm64.c
@@ -15,3 +15,13 @@ moonbit_tonyfettes_go_libc_open(
   }
   return fd;
 }
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_tonyfettes_go_libc_fcntl(int32_t fd, int32_t cmd, int32_t arg) {
+  int32_t r = fcntl(fd, cmd, arg);
+  if (r == -1) {
+    return -errno;
+  }
+  return r;
+}
