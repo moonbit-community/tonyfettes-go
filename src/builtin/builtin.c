d6603415543a47a39c6cdfc2e9d4b09e88ede02b
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-09-23 11:12:58 +0800

    refactor(builtin): take apart builtin.mbt

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -8,7 +8,7 @@ moonbit_tonyfettes_go_builtin_uintptr_from_uint64(uint64_t v) {
 
 MOONBIT_FFI_EXPORT
 void *
-moonbit_tonyfettes_go_builtin_uintptr_from_int(int32_t v) {
+moonbit_tonyfettes_go_builtin_uintptr_from_uint(uint32_t v) {
   return (void *)(uintptr_t)v;
 }
 
@@ -19,7 +19,7 @@ moonbit_tonyfettes_go_builtin_uintptr_to_uint64(void *p) {
 }
 
 MOONBIT_FFI_EXPORT
-int32_t
-moonbit_tonyfettes_go_builtin_uintptr_to_int(void *p) {
-  return (int32_t)(uintptr_t)p;
+uint32_t
+moonbit_tonyfettes_go_builtin_uintptr_to_uint(void *p) {
+  return (uint32_t)(uintptr_t)p;
 }
