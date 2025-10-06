b8686ecece74f1e582a1b8d1b4fa99d4c7eebb00
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-10-06 13:37:01 +0800

    refactor: use testdata from go

--- src/debug/macho/data/library.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int multiply(int a, int b) { return a * b; }
-
-static int counter = 0;
-
-int increment_counter() { return ++counter; }
-
-void print_info(const char *message) { printf("Library says: %s\n", message); }
